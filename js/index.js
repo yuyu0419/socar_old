33d8c82cf778329d8120f830573f143c10e12fde
gloria <yuyu04190@gmail.com>
2020-12-03 12:02:21 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -107,7 +107,7 @@ window.addEventListener('DOMContentLoaded', function () {
         setTimeout(function () {
             window.scrollTo(0, 0);
             main.style = "transform:translate(0%," + num + "px);"
-        }, 100);
+        }, 0);
     }
 
     indi.forEach(function (v, idx) {
